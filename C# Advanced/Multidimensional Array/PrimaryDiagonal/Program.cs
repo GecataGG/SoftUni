d4288ca7065f240ed7412be97a1a1f93b57a958b
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-10-10 20:38:17 +0300

    Add files via upload

---
+++ C# Advanced/Multidimensional Array/PrimaryDiagonal/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int j = 0; j < n; j++) matrix[i, j] = data[j];
+            }
+
+            int sum = 0;
+            for (int i = 0; i < n; i++) sum += matrix[i, i];
+
+            Console.WriteLine(sum);
+        }
+    }
+}
