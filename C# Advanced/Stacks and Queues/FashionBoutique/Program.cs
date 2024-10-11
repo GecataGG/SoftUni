8cac536dbfbce4071bc09c0d26afc25cc74423b2
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-10-11 18:24:40 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/FashionBoutique/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> boxesOfClothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            int remainingCapacity = rackCapacity;
+            int racks = 1;
+
+            while (boxesOfClothes.Count > 0)
+            {
+                int clothes = boxesOfClothes.Pop();
+
+                if (clothes > remainingCapacity)
+                {
+                    remainingCapacity = rackCapacity;
+                    racks++;
+                }
+
+                remainingCapacity -= clothes;
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
