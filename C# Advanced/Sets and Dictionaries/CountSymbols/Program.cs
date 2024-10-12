48728e8e6a2254ee263af9508495ef3221ce3f6b
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-10-12 20:23:41 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries/CountSymbols/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Dictionary<char, int> symbolsCounter = new Dictionary<char, int>();
+            foreach (char symbol in text)
+            {
+                if (!symbolsCounter.ContainsKey(symbol)) symbolsCounter[symbol] = 0;
+                symbolsCounter[symbol]++;
+            }
+
+            foreach (var (symbol, count) in symbolsCounter.OrderBy(x => x.Key))
+                Console.WriteLine($"{symbol}: {count} time/s");
+        }
+    }
+}
