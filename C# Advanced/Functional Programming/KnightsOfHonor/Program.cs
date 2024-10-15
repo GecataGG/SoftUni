45531ffdd9e3bce064393604f5273f4adeda870e
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-10-15 21:33:56 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming/KnightsOfHonor/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> print = x => Console.WriteLine($"Sir {x}");
+            Array.ForEach(words, print);
+        }
+    }
+}
