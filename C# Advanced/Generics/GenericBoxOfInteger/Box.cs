14961d70330ff271d1508ef42336328e0e348ba3
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-10-15 21:45:50 +0300

    Add files via upload

---
+++ C# Advanced/Generics/GenericBoxOfInteger/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfInteger;
+
+public class Box<TValue>
+{
+    public Box(TValue value)
+    {
+        this.Value = value;
+    }
+
+    public TValue Value { get; }
+
+    public override string ToString() => $"{typeof(TValue)}: {this.Value}";
+}
