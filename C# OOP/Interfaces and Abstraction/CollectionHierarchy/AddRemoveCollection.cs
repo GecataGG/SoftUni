05c316ffb8fa8f550fb87cbacdeb41cf8e252ea3
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-08 10:32:00 +0200

    Add files via upload

---
+++ C# OOP/Interfaces and Abstraction/CollectionHierarchy/AddRemoveCollection.cs
@@ -0,0 +1,18 @@
+﻿namespace CollectionHierarchy;
+
+using System.Collections.Generic;
+using CollectionHierarchy.Interfaces;
+using global::CollectionHierarchy.Interfaces;
+
+public class AddRemoveCollection : ISupportsAdd, ISupportsRemove
+{
+    private readonly Queue<string> _queue = new();
+
+    public int Add(string item)
+    {
+        this._queue.Enqueue(item);
+        return 0;
+    }
+
+    public string Remove() => this._queue.Dequeue();
+}
