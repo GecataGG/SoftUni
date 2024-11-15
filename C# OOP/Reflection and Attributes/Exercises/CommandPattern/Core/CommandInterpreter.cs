8a0f42fd0854b5fba432d9e961f53d2848ec5fb3
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-15 08:27:52 +0200

    Add files via upload

---
+++ C# OOP/Reflection and Attributes/Exercises/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,22 @@
+﻿namespace CommandPattern.Core;
+
+using System.Reflection;
+using System.Windows.Input;
+using CommandPattern.Core.Contracts;
+
+public class CommandInterpreter : ICommandInterpreter
+{
+    public string Read(string args)
+    {
+        var data = args.Split();
+
+        var assembly = Assembly.GetCallingAssembly();
+        var commandType = assembly.GetTypes().FirstOrDefault(x => typeof(ICommand).IsAssignableFrom(x) && x.Name.StartsWith(data[0]));
+        if (commandType is null) throw new InvalidOperationException("Invalid command");
+
+        var commandObj = Activator.CreateInstance(commandType);
+        if (commandObj is System.Windows.Input.ICommand command) return command.Execute(data[1..]);
+
+        return string.Empty;
+    }
+}
