4c503ebcb5dfe3f9767c8e971808656107c28f21
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-15 08:33:32 +0200

    Add files via upload

---
+++ C# OOP/Reflection and Attributes/Labs/AuthorProblem/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthorProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
