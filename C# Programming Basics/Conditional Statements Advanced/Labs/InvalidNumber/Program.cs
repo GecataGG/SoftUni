0d0f4fb023d4201631fbd528f55b5abf9ffb2950
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-16 18:07:33 +0200

    Add files via upload

---
+++ C# Programming Basics/Conditional Statements Advanced/Labs/InvalidNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace InvalidNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            bool isValid = number == 0 || (100 <= number && number <= 200);
+            if (!isValid) Console.WriteLine("invalid");
+        }
+    }
+}
