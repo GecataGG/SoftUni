4512e95cce0cb8294e7637a3172b880af4ce8343
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-22 14:04:19 +0200

    Add files via upload

---
+++ C# Programming Basics/Conditional Statements Advanced/Labs/WorkingHours/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace WorkingHours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            bool isWorkingDay = day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday" || day == "Saturday";
+            bool isWorkingHour = 10 <= hour && hour <= 18;
+
+            if (isWorkingDay && isWorkingHour) Console.WriteLine("open");
+            else Console.WriteLine("closed");
+        }
+    }
+}
