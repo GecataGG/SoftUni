3985efd1bffe8d5c4a60656d8fd53c21e4604252
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-22 14:14:26 +0200

    Add files via upload

---
+++ C# Programming Basics/Conditional Statements/Exercises/WorldSwimmingRecord/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace WorldSwimmingRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double existingRecordInSeconds = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+
+            double totalTimeInSeconds = distanceInMeters * timePerMeter + Math.Floor(distanceInMeters / 15) * 12.5;
+            if (totalTimeInSeconds < existingRecordInSeconds) Console.WriteLine($"Yes, he succeeded! The new world record is {totalTimeInSeconds:f2} seconds.");
+            else Console.WriteLine($"No, he failed! He was {totalTimeInSeconds - existingRecordInSeconds:f2} seconds slower.");
+        }
+    }
+}
