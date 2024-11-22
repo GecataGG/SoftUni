e83cb24a06dca0aab99547bb0a9eb98826088c43
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-22 14:07:43 +0200

    Add files via upload

---
+++ C# Programming Basics/Conditional Statements/Exercises/Add15Minutes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Add15Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 15;
+            if (minutes >= 60)
+            {
+                minutes -= 60;
+
+                hour += 1;
+                if (hour == 24) hour = 0;
+            }
+
+            if (minutes < 10) Console.WriteLine($"{hour}:0{minutes}");
+            else Console.WriteLine($"{hour}:{minutes}");
+        }
+    }
+}
