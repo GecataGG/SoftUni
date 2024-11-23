9e4f1bb69a8fdd95b598fd39dbc64df119cce6a4
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-23 11:59:38 +0200

    Add files via upload

---
+++ C# Programming Basics/Conditional Statements/Labs/AnimalType/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AnimalType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+
+            if (animal == "dog") Console.WriteLine("mammal");
+            else if (animal == "crocodile" || animal == "tortoise" || animal == "snake") Console.WriteLine("reptile");
+            else Console.WriteLine("unknown");
+        }
+    }
+}
