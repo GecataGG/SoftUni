880b8f1421b4271c1c1b198553186c0c202b0922
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-25 19:02:48 +0200

    Add files via upload

---
+++ C# Programming Basics/Exam Preparation/CatWalking/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CatWalking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int durationInMinutes = int.Parse(Console.ReadLine());
+            int walksCount = int.Parse(Console.ReadLine());
+            int calories = int.Parse(Console.ReadLine());
+
+            int burnedCalories = walksCount * durationInMinutes * 5;
+            if (burnedCalories * 2 >= calories) Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {burnedCalories}.");
+            else Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {burnedCalories}.");
+        }
+    }
+}
