8bbc4a51ccd7958a20601699b909fdcd1cfce117
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-26 17:20:33 +0200

    Add files via upload

---
+++ C# Programming Basics/For loops/Labs/EvenPowersOf2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace EvenPowersOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0, val = 1; i <= n; i += 2, val *= 4) Console.WriteLine(val);
+        }
+    }
+}
