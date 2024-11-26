d8aeb59a9750cc24660a1ff992d85959ac9e3797
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-11-26 17:14:24 +0200

    Add files via upload

---
+++ C# Programming Basics/For loops/Exercises/CleverLily/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace CleverLily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            int savedMoney = 0, moneyToReceive = 10;
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    savedMoney += moneyToReceive - 1;
+                    moneyToReceive += 10;
+                }
+                else { savedMoney += toyPrice; }
+            }
+
+            if (savedMoney >= washingMachinePrice) Console.WriteLine($"Yes! {savedMoney - washingMachinePrice:f2}");
+            else Console.WriteLine($"No! {washingMachinePrice - savedMoney:f2}");
+        }
+    }
+}
