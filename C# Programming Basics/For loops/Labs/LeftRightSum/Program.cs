8f0c5140358776c3fd157f265c9835a484a59a90
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-12-02 22:44:00 +0200

    Add files via upload

---
+++ C# Programming Basics/For loops/Labs/LeftRightSum/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int leftSum = 0;
+        for (int i = 0; i < n; i++) leftSum += int.Parse(Console.ReadLine());
+
+        int rightSum = 0;
+        for (int i = 0; i < n; i++) rightSum += int.Parse(Console.ReadLine());
+
+        if (leftSum == rightSum) Console.WriteLine($"Yes, sum = {leftSum}");
+        else Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+    }
+}
