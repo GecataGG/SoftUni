a3f63696a6754cdcd4ab1ab80fe470676bd101ae
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-12-02 22:51:21 +0200

    Add files via upload

---
+++ C# Programming Basics/For loops/Labs/SumNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int currentNumber = int.Parse(Console.ReadLine());
+            sum += currentNumber;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
