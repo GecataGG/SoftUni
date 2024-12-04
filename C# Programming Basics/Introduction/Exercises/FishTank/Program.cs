f4c000e0ca2e426081218e61c8cc6e12c59f718a
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-12-04 17:07:02 +0200

    Add files via upload

---
+++ C# Programming Basics/Introduction/Exercises/FishTank/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int length = int.Parse(Console.ReadLine());
+        int width = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+        double percentage = double.Parse(Console.ReadLine());
+
+        double volume = 0.001 * length * width * height;
+        double freeSpace = volume * (1 - 0.01 * percentage);
+        Console.WriteLine(freeSpace);
+    }
+}
