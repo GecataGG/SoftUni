6e141b260ab2d5ea1aa95ccd494cbe687e233aa9
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-12-04 17:06:19 +0200

    Add files via upload

---
+++ C# Programming Basics/Introduction/Exercises/DepositsCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double depositSum = double.Parse(Console.ReadLine());
+        int depositLength = int.Parse(Console.ReadLine());
+        double yearlyRate = double.Parse(Console.ReadLine());
+
+        double result = depositSum + depositLength * (depositSum * yearlyRate * 0.01 / 12);
+        Console.WriteLine(result);
+    }
+}
