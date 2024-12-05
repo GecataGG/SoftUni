ed0c1236599bae094fca652cc3436708a8d0975b
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-12-05 15:34:07 +0200

    Add files via upload

---
+++ C# Programming Basics/Introduction/Labs/RectangleArea/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int area = a * b;
+        Console.WriteLine(area);
+    }
+}
