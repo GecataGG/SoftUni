91c6112d1d6684a0d8c61b67138c9eee120e89db
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-12-05 15:34:47 +0200

    Add files via upload

---
+++ C# Programming Basics/Introduction/Labs/YardGreening/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const double pricePerSquareMeter = 7.61;
+        const double discountPercentage = 0.18;
+
+        double squareMeters = double.Parse(Console.ReadLine());
+
+        double totalCosts = squareMeters * pricePerSquareMeter;
+        double discount = discountPercentage * totalCosts;
+        double finalPrice = totalCosts - discount;
+
+        Console.WriteLine($"The final price is: {finalPrice} lv.");
+        Console.WriteLine($"The discount is: {discount} lv.");
+    }
+}
