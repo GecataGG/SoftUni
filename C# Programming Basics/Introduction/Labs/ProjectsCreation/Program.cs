32cfa6b67c71bcfd1725c40fd26794debcd551e4
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-12-05 15:33:31 +0200

    Add files via upload

---
+++ C# Programming Basics/Introduction/Labs/ProjectsCreation/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string architectName = Console.ReadLine();
+        int projectsCount = int.Parse(Console.ReadLine());
+
+        int requiredHours = projectsCount * 3;
+        Console.WriteLine($"The architect {architectName} will need {requiredHours} hours to complete {projectsCount} project/s.");
+    }
+}
