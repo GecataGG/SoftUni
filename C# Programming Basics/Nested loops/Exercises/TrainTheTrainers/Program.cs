2e58a13dcb835c0b9c82a30432b22bf7a41e9593
Georgi Georgiev <94467763+GecataGG@users.noreply.github.com>
2024-12-14 18:35:26 +0200

    Add files via upload

---
+++ C# Programming Basics/Nested loops/Exercises/TrainTheTrainers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int juries = int.Parse(Console.ReadLine());
+
+            double presentationScoresSum = 0;
+            int presentationsCount = 0;
+
+            string presentationName = Console.ReadLine();
+            while (presentationName != "Finish")
+            {
+                double currentScoresSum = 0;
+                for (int i = 0; i < juries; i++)
+                {
+                    double juryScore = double.Parse(Console.ReadLine());
+                    currentScoresSum += juryScore;
+                }
+
+                double grade = currentScoresSum / juries;
+                Console.WriteLine($"{presentationName} - {grade:f2}.");
+
+                presentationScoresSum += grade;
+                presentationsCount++;
+
+                presentationName = Console.ReadLine();
+            }
+
+            double averageAssessment = presentationScoresSum / presentationsCount;
+            Console.WriteLine($"Student's final assessment is {averageAssessment:f2}.");
+        }
+    }
+}
